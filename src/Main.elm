acf0681084b277c8caf64bc5cb515b551ae85f5e
rinarudhei <rinaldiadrian5@gmail.com>
2026-06-10 22:03:12 +0700

    feat: color system
    add white, black, gray, pink, red, gradient1, gradient2, gradient3
    colors

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,50 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Element exposing (Element, rgb255)
+import Element.Background
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
+
+
+
+---- DESIGN SYSTEM ----
+
+
+white : Element.Color
+white =
+    rgb255 255 255 255
+
+
+black : Element.Color
+black =
+    rgb255 0 0 0
+
+
+red : Element.Color
+red =
+    rgb255 249 100 100
+
+
+pink : Element.Color
+pink =
+    rgb255 206 152 152
+
+
+gradient1 : { angle : Float, steps : List Element.Color }
+gradient1 =
+    { angle = -1.466, steps = [ rgb255 248 191 191, rgb255 238 139 139 ] }
+
+
+gradient2 : { angle : Float, steps : List Element.Color }
+gradient2 =
+    { angle = -1.3788, steps = [ white, rgb255 255 244 244 ] }
+
+
+gradient3 : { angle : Float, steps : List Element.Color }
+gradient3 =
+    { angle = 1.44862, steps = [ rgb255 255 244 244, white ] }
+
 
 
 ---- MODEL ----
