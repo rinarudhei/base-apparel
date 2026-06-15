b5638df7124d8bcd66dff3b7347c957544486057
rinarudhei <rinaldiadrian5@gmail.com>
2026-06-15 11:43:18 +0700

    prepare assets for github pages

---
+++ precache-manifest.d10e33b9bb3a09c1aec5317eca30019c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07f423b8bbf4a50c2c449b96795fcb6",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "4ba847c4f316bb5edee60b72daab2a76",
+    "url": "/fonts/josefin-sans-v34-latin-300.woff2"
+  },
+  {
+    "revision": "4b7c8f754fbaf01e7e2eb27e01742974",
+    "url": "/fonts/josefin-sans-v34-latin-500.woff2"
+  },
+  {
+    "revision": "ccd50708b487ce4ec0a8c9b249e2ec88",
+    "url": "/fonts/josefin-sans-v34-latin-regular.woff2"
+  },
+  {
+    "revision": "9a99295173be6c0f6dbc1e173d75ce48",
+    "url": "/images/bg-pattern-desktop.svg"
+  },
+  {
+    "revision": "fc4364e6a1779c0325b076a4b770fa2e",
+    "url": "/images/hero-desktop.jpg"
+  },
+  {
+    "revision": "e02f5666a072a36ab632face99bf946a",
+    "url": "/images/hero-mobile.jpg"
+  },
+  {
+    "revision": "f4f1c6c8818d8d71e85775ccaab5e52e",
+    "url": "/images/hero-tablet.jpg"
+  },
+  {
+    "revision": "69950c2de382a8d4924260343e516fad",
+    "url": "/images/icon-arrow.svg"
+  },
+  {
+    "revision": "337809979308399afb36fa5f0e3fbcdf",
+    "url": "/images/icon-error.svg"
+  },
+  {
+    "revision": "877543ce69f4ed6f834f26e7dc4ca930",
+    "url": "/images/logo.svg"
+  },
+  {
+    "revision": "d5774a335b539991d21564af3527391f",
+    "url": "/images/screenshot1.png"
+  },
+  {
+    "revision": "6c1d3a47c2ac1c1e34ecdf0f4235f08a",
+    "url": "/images/screenshot2.png"
+  },
+  {
+    "revision": "952940ce08ea9799275cc43a3b3b416d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc1db46c53f57da0fae465b3e694c65",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "20ff90ebc0b23646b12d",
+    "url": "/static/css/main.d6d9f280.chunk.css"
+  },
+  {
+    "revision": "20ff90ebc0b23646b12d",
+    "url": "/static/js/main.20ff90eb.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "ded87515c392c3502f6c",
+    "url": "/static/js/vendors~main.ded87515.chunk.js"
+  },
+  {
+    "revision": "4ba847c4f316bb5edee60b72daab2a76",
+    "url": "/static/media/josefin-sans-v34-latin-300.61225cd8.woff2"
+  },
+  {
+    "revision": "4b7c8f754fbaf01e7e2eb27e01742974",
+    "url": "/static/media/josefin-sans-v34-latin-500.c293c0dd.woff2"
+  },
+  {
+    "revision": "ccd50708b487ce4ec0a8c9b249e2ec88",
+    "url": "/static/media/josefin-sans-v34-latin-regular.237687e1.woff2"
+  }
+]);
